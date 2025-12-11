7fbbce3e529477369463c3bfe32fb8e30f4220a4
Howoo <jhoworus@gmail.com>
2025-12-11 23:19:22 +0100

    Dodanie do Python Clienta tworzenia math expression

--- PythonClient/main.py
+++ PythonClient/main.py
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
-import io
-=======
->>>>>>> d9186c7 (python test code)
 import sys
 from Enums import DataTypes
 from Variables import VariablesStore
 from DumpVariables import VariablesDump
-<<<<<<< HEAD
-from pprint import pprint
-
-def main():
-    store = VariablesStore()
-    store.add_array("volume_double", DataTypes.DATA_D, [8]*27, [3,3,3])
-    store.add_array("matrix_A", DataTypes.DATA_UI16, [10, 20, 30, 40], [2, 2])
-    store.add_array("linear_A", DataTypes.DATA_UI16, [1, 2, 3, 4], [4])
-    store.add_array("volume_A", DataTypes.DATA_UI16, [1]*8, [2, 2, 2])
-    store.add_array("linear_B", DataTypes.DATA_UI16, [8, 9], [2])
-    store.add_scalar("scalarA", DataTypes.DATA_UI16, 10)
-    store.add_scalar("scalarB", DataTypes.DATA_UI16, 11)
-    store.add_array("linear_C", DataTypes.DATA_UI16, [8, 9], [4])
-    store.add_array("linear_Double", DataTypes.DATA_D, [8]*40, [40])
-
-    export  = store.get_export_map()
-    pprint(export)
-    
-    filename = "valuedump.txt"
-    print(f"\n[3] Dumping to '{filename}' for manual check...")
-=======
 from References import Ref
+from MathExpressionBuilder import MathExpression
 
 def main():
     print("=== TEST: DUMP + DEEP NESTED REF HEX ===")
@@ -63,13 +39,10 @@ def main():
     # ------------------------------------------------
     filename = "valuedump.txt"
     print(f"Dumping variables to '{filename}'...")
->>>>>>> d9186c7 (python test code)
     
     dumper = VariablesDump(store)
     with open(filename, "w") as f:
         dumper.write(f)
-<<<<<<< HEAD
-=======
     print("Dump Complete.")
 
     # 3. TEST 1: STANDARD NESTED REFERENCE
@@ -98,7 +71,6 @@ def main():
 
     print(f"Ref: target_data[ lookup_table[ deep_selector ] ]")
     print(f"Hex: {ref2}")
->>>>>>> d9186c7 (python test code)
 
 if __name__ == "__main__":
     main()
