0b4bcbf4d1b6f832c30593a9c6b0bdbbe232cf80
Howoo <jhoworus@gmail.com>
2025-12-12 09:59:22 +0100

    Added math parser to the new files scheme

--- components/emulator/blocks/block_math.c
+++ components/emulator/blocks/block_math.c
@@ -1,24 +1,193 @@
 #include "block_math.h"
+#include "emulator_errors.h"
+#include <math.h>
+#include <float.h>
+#include <stdbool.h>
 
 int cnt;
-emu_err_t block_math(block_handle_t* block_data){
-    double val ;
-    // uint8_t table[3]= {0xFF,0xFF,0xFF};
-    // //ESP_LOGI("GLOBALACCES","resuld by hand %d",MEM_GET_U8(0,table));
+extern block_handle_t **emu_block_struct_execution_list;
+static inline bool is_greater(double a, double b);
+static inline bool is_equal(double a, double b);
+static inline bool is_zero(double a);
 
-    // uint8_t table2[3]= {0x01,0xFF,0xFF};
-    // //ESP_LOGI("GLOBALACCES","res uld by hand2 %d",MEM_GET_U16(0,table2));
-    double result = 0.0;
-    uint8_t table[3]= {255,255,255};
-    ESP_LOGI("normall access","result1 %ld",MEM_GET_U32(0, table));
-    utils_global_var_acces_recursive(block_data->global_reference[0], &result);
-    ESP_LOGI("GLOBALACCES","result1 %lf", result);
-    utils_global_var_acces_recursive(block_data->global_reference[1], &result);
-    ESP_LOGI("GLOBALACCES","result2 %lf", result);
-        // utils_global_var_acces_recursive(test, &result);
-        // utils_global_var_acces_recursive(test, &result);
-        // utils_global_var_acces_recursive(test, &result);
+
+emu_err_t emu_parse_math_block(chr_msg_buffer_t *source, size_t msg_index)
+{
+    uint8_t *data;
+    size_t len;
+    size_t const_msg_cnt = 1;
+    block_handle_t *block_ptr;
+
+    chr_msg_buffer_get(source, msg_index, &data, &len);
+    uint16_t block_id = READ_U16(data, 3);
+
+    block_ptr = emu_block_struct_execution_list[block_id];
+
+    block_ptr->extras = calloc(1, sizeof(expression_t));
+    
+    __emu_parse_math_const(source, msg_index, (block_ptr->extras), &const_msg_cnt);
+    __emu_parse_math_expr(source, msg_index+const_msg_cnt, (block_ptr->extras));
+    
+    return EMU_OK;
+}
+
+emu_err_t __emu_parse_math_expr(chr_msg_buffer_t *source, size_t msg_index, expression_t* expression)
+{
+    uint8_t *data;
+    size_t len;
+    size_t len_total;
+    uint8_t op_count = 0;
+    uint8_t idx_start = 0;
+
+    chr_msg_buffer_get(source, msg_index, &data, &len);
+    op_count = data[5];
+    expression->count = op_count;
+    expression->code = (instruction_t*)calloc(op_count, sizeof(instruction_t));
+    len_total = op_count * 2 + 1;
+    __emu_parse_math_expr_msg(data, len, 6, &idx_start, expression->code);
+    
+    while(len_total>(len-5)){
+        len_total = len_total - (len - 5);
+        chr_msg_buffer_get(source, msg_index++, &data, &len);
+        __emu_parse_math_expr_msg(data, len, 5, &idx_start, expression->code);
+    }
+
+    return EMU_OK;
+}
+
+emu_err_t __emu_parse_math_expr_msg(uint8_t *data, uint16_t len, size_t start_index, uint8_t *idx_start, instruction_t* code)
+{
+    for(int i = start_index; i<len; i+=2){
+        memcpy(&(code[(*idx_start)].op), &data[i], 1);
+        memcpy(&(code[(*idx_start)].input_index), &data[i+1], 1);
+        (*idx_start)++;
+    }
+    return EMU_OK;
+}
+
+emu_err_t __emu_parse_math_const(chr_msg_buffer_t *source, size_t msg_index, expression_t* expression, size_t *const_msg_cnt){
+
+    uint8_t *data;
+    size_t len;
+    size_t len_total;
+    uint8_t const_cnt = 0;
+    uint8_t idx_start = 0;
+
+    chr_msg_buffer_get(source, msg_index, &data, &len);
+    const_cnt = data[5];
+    len_total = const_cnt * sizeof(double) + 1;
+    expression->constant_table = (double*)calloc(const_cnt, sizeof(double));
+    __emu_parse_math_const_msg(data, len, 6, &idx_start, expression->constant_table);
+
+    while(len_total>(len-5)){
+        len_total = len_total - (len - 5);
+        chr_msg_buffer_get(source, msg_index++, &data, &len);
+        __emu_parse_math_const_msg(data, len, 5, &idx_start, expression->constant_table);
+        (*const_msg_cnt)++;
+    }
+    return EMU_OK;
+}
+
+
+emu_err_t __emu_parse_math_const_msg(uint8_t *data, uint16_t len, size_t start_index, uint8_t *idx_start, double* table){
+    for(size_t i = start_index; i<len; i=i+sizeof(double)){
+        memcpy(&(table[*idx_start]), &data[i], sizeof(double));
+        (*idx_start)++;
+    }
+    return EMU_OK;
+}
+
+
+emu_err_t block_math(block_handle_t* block){
+    // double val ;
+    // // uint8_t table[3]= {0xFF,0xFF,0xFF};
+    // // //ESP_LOGI("GLOBALACCES","resuld by hand %d",MEM_GET_U8(0,table));
+
+    // // uint8_t table2[3]= {0x01,0xFF,0xFF};
+    // // //ESP_LOGI("GLOBALACCES","res uld by hand2 %d",MEM_GET_U16(0,table2));
+    // double result = 0.0;
+    // uint8_t table[3]= {255,255,255};
+    // ESP_LOGI("normall access","result1 %ld",MEM_GET_U32(0, table));
+    // utils_global_var_acces_recursive(block_data->global_reference[0], &result);
+    // ESP_LOGI("GLOBALACCES","result1 %lf", result);
+    // utils_global_var_acces_recursive(block_data->global_reference[1], &result);
+    // ESP_LOGI("GLOBALACCES","result2 %lf", result);
+    //     // utils_global_var_acces_recursive(test, &result);
+    //     // utils_global_var_acces_recursive(test, &result);
+    //     // utils_global_var_acces_recursive(test, &result);
        
-    block_pass_results(block_data);
+    expression_t* eval = (expression_t*)block->extras;
+    double stack[16];
+    int over_top = 0;
+    double result = 0;
+
+    for(int i = 0; i<eval->count; i++){
+        instruction_t *ins = &(eval->code[i]);
+        switch(ins->op){
+            case OP_VAR:
+                utils_get_in_val_autoselect(ins->input_index, block, &stack[over_top++]);
+                break;
+
+            case OP_CONST:
+                stack[over_top++] = eval->constant_table[ins->input_index];
+                break;
+
+            case OP_ADD:
+                stack[over_top-2] = stack[over_top-2] + stack[over_top-1];
+                over_top--;
+                break;
+
+            case OP_MUL:
+                stack[over_top-2] = stack[over_top-2] * stack[over_top-1];
+                over_top--;
+                break;
+
+            case OP_DIV:
+                if(is_zero(stack[over_top-1]))
+                    return EMU_ERR_DIV_BY_ZERO;
+
+                stack[over_top-2] = stack[over_top-2]/stack[over_top-1];
+                over_top--;
+                break;
+
+            case OP_COS:
+                stack[over_top-1] = cos(stack[over_top-1]);
+                break;
+
+            case OP_SIN:
+                stack[over_top-1] = sin(stack[over_top-1]);
+                break; 
+
+            case OP_POWER:
+                stack[over_top-2] = pow(stack[over_top-2], stack[over_top-1]);
+                over_top--;
+                break;
+
+            case OP_ROOT:
+                stack[over_top-1] = sqrt(stack[over_top-1]);
+                break;
+
+            case OP_SUB:
+                stack[over_top-2] = stack[over_top-2] - stack[over_top-1];
+                over_top--;
+                break; 
+        }
+    }
+
+    result = stack[0];
+    utils_set_q_val(block, 0, (void*)&result);
+    block_pass_results(block);
     return EMU_OK;      
-}   
+} 
+
+static inline bool is_equal(double a, double b){
+    return fabs(a-b)<DBL_EPSILON;
+}
+
+static inline bool is_zero(double a){
+    return fabs(a)<DBL_EPSILON;
+}
+
+static inline bool is_greater(double a, double b){
+    return fabs(a-b)>DBL_EPSILON;
+}
--- components/emulator/blocks/include/block_math.h
+++ components/emulator/blocks/include/block_math.h
@@ -1,11 +1,42 @@
 #include "utils_block_in_q_access.h"
 #include "utils_global_access.h"
 
+#define READ_U16(DATA, IDX)     ((uint16_t)((DATA)[(IDX)]) | ((uint16_t)((DATA)[(IDX)+1]) << 8))
 
+typedef enum {
+    OP_VAR = 0x00,
+    OP_ADD = 0x01,
+    OP_MUL = 0x02,
+    OP_DIV = 0x03,
+    OP_SUB = 0x04,
+    OP_ROOT = 0x05,
+    OP_POWER = 0x06,
+    OP_SIN = 0x07,
+    OP_COS = 0x08,
+    OP_CONST = 0x09,
+} op_code_t;
+
+typedef struct {
+    op_code_t op;
+    uint8_t input_index; //for operator its 00, for constant index in constant table
+} instruction_t;
+
+
+typedef struct{
+    instruction_t *code;
+    uint8_t count;
+    double *constant_table;
+} expression_t;
+
+typedef emu_err_t (*emu_block_func)(block_handle_t *block);
+
+emu_err_t emu_parse_math_block(chr_msg_buffer_t *source, size_t msg_index);
+emu_err_t __emu_parse_math_expr(chr_msg_buffer_t *source, size_t msg_index, expression_t* expression);
+emu_err_t __emu_parse_math_expr_msg(uint8_t *data, uint16_t len, size_t start_index, uint8_t *idx_start, instruction_t* code);
+emu_err_t __emu_parse_math_const(chr_msg_buffer_t *source, size_t msg_index, expression_t* expression, size_t *const_msg_cnt);
+emu_err_t __emu_parse_math_const_msg(uint8_t *data, uint16_t len, size_t start_index, uint8_t *idx_start, double* table);
 
 /**
 *@brief compute block - handling math operations
 */
 emu_err_t block_math(block_handle_t *src);
-
-
